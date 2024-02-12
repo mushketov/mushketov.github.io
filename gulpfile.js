af548f55149147b8f3248574f954c46ba0c5e048
Mushketov <rootanswer@gmail.com>
2024-02-12 05:49:27 +0300

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,16 @@
 const gulp = require('gulp')
 const browserSync = require('browser-sync').create()
-
+const watch = require('gulp-watch')
+gulp.task('watch', function () {
+	watch(['./src/*.html', 'src/css/**/*css'], gulp.parallel(browserSync.reload))
+})
 // Static server
 gulp.task('server', function () {
 	browserSync.init({
 		server: {
-			baseDir: './',
+			baseDir: './src/',
+			notify: false
 		},
 	})
 })
+gulp.task('default', gulp.parallel('server', 'watch'))
