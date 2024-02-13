2c9f3bf87076e930a0aec8b81fba1fb7b9098349
Mushketov <rootanswer@gmail.com>
2024-02-14 01:54:39 +0300

    refactor, add new projects

---
+++ src/pets/neapol-food/js/main.js
@@ -0,0 +1,7 @@
+$(function(){
+  $('.content-wrap').slick({
+    autoplay: true,
+    dots: true,
+    arrows: false
+  })
+})
---
+++ src/pets/uber/js/app.js
@@ -0,0 +1,2 @@
+const answer = +prompt('сколько лет', 18)
+console.log(answer + 5)
